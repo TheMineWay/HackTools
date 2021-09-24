17598dff7ab49c806d3fe7f2f616345506884278
Joel Campos <joelcamposoliva9000@gmail.com>
2021-09-24 23:02:55 +0200

    SSH Connection (IP)

--- HackTools/Services/SSH/SSHConnection.cs
+++ HackTools/Services/SSH/SSHConnection.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Renci.SshNet;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 namespace HackTools
 {
@@ -17,14 +19,38 @@ namespace HackTools
         {
             // UI
             Console.Clear();
-            Printer.Print("&cyan;\tDestination: ", newLine: false);
             string ip;
             do
             {
+                Printer.Print("&cyan;\tDestination: ", newLine: false);
                 ip = Console.ReadLine();
                 Console.Clear();
-                Printer.Print("&blue;\t[-] Checking");
+                LoadingUI loading = new LoadingUI($"Checking IP ({ip})");
+                PingReply reply = null;
+                Task ping = Task.Run(async () => {
+                    Ping ping = new Ping();
+                    reply = ping.Send(ip);
+                });
+                int times = 0;
+                while (reply == null)
+                {
+                    System.Threading.Thread.Sleep(200);
+                    times++;
+                    loading.Print();
+                    if (times >= 20) break;
+                }
+                if(times < 20)
+                {
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        Printer.Print("&green; Successful IP check");
+                        break;
+                    }
+                }
+                Printer.Print("&red; [!] The IP check has failed");
             } while (true);
+            // Credentials
+
         }
 
         public SSHConnection(string username, string password, string ip, string name = "SSH Connection")
