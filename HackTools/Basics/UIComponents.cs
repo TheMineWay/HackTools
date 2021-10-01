4c91116c73be6e9eabd0381b4ec8842fe459752b
Joel Campos <joelcamposoliva9000@gmail.com>
2021-10-01 22:23:27 +0200

    Fancy title

--- HackTools/Basics/UIComponents.cs
+++ HackTools/Basics/UIComponents.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace HackTools
 {
@@ -45,6 +46,21 @@ namespace HackTools
                 if (res == ConsoleKey.N) return false;
                 if (clearOnFail) Console.Clear();
             } while (true);
+        }
+
+        public static void CenterAndPrint(string[] lines)
+        {
+            foreach (string line in lines.Select((e) => Center(e)).ToArray()) Console.WriteLine(line);
+        }
+
+        public static string Center(string line)
+        {
+            int sides = Console.WindowWidth - line.Length;
+            if (sides <= 1) return line;
+
+            sides /= 2;
+
+            return Printer.Fill(" ", sides) + line + Printer.Fill(" ", ((Console.WindowWidth - line.Length) % 2 == 0 ? sides : sides - 1));
         }
     }
 }
--- HackTools/Program.cs
+++ HackTools/Program.cs
@@ -8,10 +8,37 @@ namespace HackTools
         static void Main(string[] args)
         {
             Console.Title = "Hack Tools - By TheMineWay";
+
+            if (Console.WindowWidth >= 76)
+            {
+                UIComponents.CenterAndPrint("\n██╗░░██╗░█████╗░░█████╗░██╗░░██╗  ████████╗░█████╗░░█████╗░██╗░░░░░░██████╗\n██║░░██║██╔══██╗██╔══██╗██║░██╔╝  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝\n███████║███████║██║░░╚═╝█████═╝░  ░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░\n██╔══██║██╔══██║██║░░██╗██╔═██╗░  ░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗\n██║░░██║██║░░██║╚█████╔╝██║░╚██╗  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝\n╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░".Split("\n"));
+                Printer.Print("&white;" + UIComponents.Center("by TheMineWay"));
+
+                Printer.Print("&cyan;\n" + UIComponents.Center("Press any key to start"));
+                Console.ReadKey();
+            }
+
+            Menu<ScreenOption>.MenuItem[] items = new Menu<ScreenOption>.MenuItem[] {
+                new Menu<ScreenOption>.MenuItem("Available tools", new StartMenuScreen())
+            };
+            Menu<ScreenOption> menu = new Menu<ScreenOption>(title: "Menu", items: items);
+            do
+            {
+                ScreenOption option = menu.DisplayMenu();
+                if (option == null) return;
+                option.Open();
+            } while (true);
+        }
+    }
+
+    class StartMenuScreen: ScreenOption
+    {
+        public override void Open()
+        {
             Menu<ScreenOption>.MenuItem[] items = new Menu<ScreenOption>.MenuItem[] {
                 new Menu<ScreenOption>.MenuItem("SSH", new SSHMenu())
             };
-            Menu<ScreenOption> menu = new Menu<ScreenOption>(title:"Menu", items: items);
+            Menu<ScreenOption> menu = new Menu<ScreenOption>(title: "Tools", items: items);
             do
             {
                 ScreenOption option = menu.DisplayMenu();
